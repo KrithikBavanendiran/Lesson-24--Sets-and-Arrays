e481157b547af8cb3929f30d00110fbcd9f52246
KrithikBavanendiran <bvino1522@gmail.com>
2025-05-07 18:10:35 +0100

    Lesson 24

---
+++ Intersection.py
@@ -0,0 +1,8 @@
+setx={"green","blue"}
+sety={"blue","yellow"}
+print("Original set elemnts: ")
+print(setx)
+print(sety)
+print("Intersection of the 2 sets")
+setz=setx.intersection(sety)
+print(setz)
---
+++ Operations on Sets.py
@@ -0,0 +1,18 @@
+my_set={1,2,3}
+print(my_set)
+
+my_set={1.0,"Hello",(1,2,3)}
+print(my_set)
+
+my_set={1,2,3,4,3,2}
+print(my_set)
+
+my_set= set([1,2,3,2])
+print(my_set,"\n")
+
+numset=set([0,1,3,4,5])
+print("Original Set: ")
+print(numset)
+numset.pop()
+print("After removing the first number from the set")
+print(numset,"\n")
